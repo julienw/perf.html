01b4fe89136088aa3996d68e25df4f6ab897ab67
Julien Wajsberg <felash@gmail.com>
2017-09-06 18:40:59 +0200

    Fix import for IDBKeyRange

--- flow-typed/overrides/Window.js
+++ flow-typed/overrides/Window.js
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 // @flow
 
-import type { IDBFactory } from '../../src/types/indexeddb';
+import type { IDBFactory, IDBKeyRange } from '../../src/types/indexeddb';
 
 type Lib = { debugName: string };
 
